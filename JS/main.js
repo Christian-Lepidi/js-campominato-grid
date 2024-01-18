809557717bc59216116fc45311b24e43bac993dd
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-18 17:34:14 +0100

    feat: add. creation grid

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,14 @@
 const playButton = document.getElementById("play");
 const gameBoard = document.getElementById("gameboard");
+
+for (i = 1; i <= 100; i++) {
+  const cell = generateCell(i);
+  gameBoard.append(cell);
+}
+
+function generateCell(i) {
+  const cell = document.createElement("div");
+  cell.classList.add("box");
+  cell.innerHTML = i;
+  return cell;
+}
