590093cacdf29c489b79866ab22646922db0fd14
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-22 17:52:56 +0100

    feat: add. three game levels

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,24 @@
 const playButton = document.getElementById("play");
 const gameBoard = document.getElementById("gameboard");
+let gameLevel = document.getElementById("game-level");
+generateGrid(gameBoard);
 
-for (i = 1; i <= 100; i++) {
-  const cell = generateCell(i);
-  gameBoard.append(cell);
+playButton.addEventListener("click", function () {
+  generateGrid(gameBoard);
+});
+
+function generateGrid(gameBoard) {
+  let cellsNumber = gameLevel.value;
+  gameBoard.innerHTML = "";
+  for (i = 1; i <= cellsNumber; i++) {
+    const cell = generateCell(i, cellsNumber);
+    gameBoard.append(cell);
+  }
 }
-
-function generateCell(i) {
+function generateCell(i, a) {
   const cell = document.createElement("div");
   cell.classList.add("box");
+  cell.classList.add("box-" + a);
   cell.innerHTML = i;
   cell.addEventListener("click", function () {
     this.classList.toggle("clicked");
